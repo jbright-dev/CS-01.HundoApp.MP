80523781a98d9344a757316078d68c8d1374cacb
Jason Bright <jason.bright@horne.com>
2023-05-10 08:09:49 -0500

    Add project files.

---
+++ Hundo/Program.cs
@@ -0,0 +1,6 @@
+﻿//Introduction
+//Retreiving values entered by the user
+//Parse the values within the string
+//Conver the text string into an integer
+//Enter the numbers into a List
+//Print the list into the console
